8b55a767ff60c88d10d6ae9befac96ee10a67f9d
Timo Furrer <tuxtimo@gmail.com>
2024-02-29 13:19:22 +0100

    tmux and ruby lsp

--- nvim/lua/timo/plugins/lsp.lua
+++ nvim/lua/timo/plugins/lsp.lua
@@ -1,3 +1,88 @@
+-------- The code below is apparently necessary until I'm using nvim 0.10.0.
+-- See https://shopify.github.io/ruby-lsp/EDITORS_md.html#label-Neovim+LSP
+
+-- textDocument/diagnostic support until 0.10.0 is released
+_timers = {}
+local function setup_diagnostics(client, buffer)
+  if require("vim.lsp.diagnostic")._enable then
+    return
+  end
+
+  local diagnostic_handler = function()
+    local params = vim.lsp.util.make_text_document_params(buffer)
+    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+      if err then
+        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+        vim.lsp.log.error(err_msg)
+      end
+      local diagnostic_items = {}
+      if result then
+        diagnostic_items = result.items
+      end
+      vim.lsp.diagnostic.on_publish_diagnostics(
+        nil,
+        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+        { client_id = client.id }
+      )
+    end)
+  end
+
+  diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_lines = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+    end,
+    on_detach = function()
+      if _timers[buffer] then
+        vim.fn.timer_stop(_timers[buffer])
+      end
+    end,
+  })
+end
+
+-- adds ShowRubyDeps command to show dependencies in the quickfix list.
+-- add the `all` argument to show indirect dependencies as well
+local function add_ruby_deps_command(client, bufnr)
+    vim.api.nvim_buf_create_user_command(bufnr, "ShowRubyDeps",
+                                          function(opts)
+
+        local params = vim.lsp.util.make_text_document_params()
+
+        local showAll = opts.args == "all"
+
+        client.request("rubyLsp/workspace/dependencies", params,
+                        function(error, result)
+            if error then
+                print("Error showing deps: " .. error)
+                return
+            end
+
+            local qf_list = {}
+            for _, item in ipairs(result) do
+                if showAll or item.dependency then
+                    table.insert(qf_list, {
+                        text = string.format("%s (%s) - %s",
+                                              item.name,
+                                              item.version,
+                                              item.dependency),
+
+                        filename = item.path
+                    })
+                end
+            end
+
+            vim.fn.setqflist(qf_list)
+            vim.cmd('copen')
+        end, bufnr)
+    end, {nargs = "?", complete = function()
+        return {"all"}
+    end})
+end
+
 return {
   {
     "williamboman/mason.nvim",
@@ -28,7 +113,8 @@ return {
           "marksman", -- markdown
           "pylsp", -- python-lsp
           "pyright",
-          "solargraph", -- For Ruby and especially GitLab development, since they have configs for it.
+          -- "solargraph", -- For Ruby and especially GitLab development, since they have configs for it.
+          "ruby_ls",
           "terraformls",
           "tsserver",
           "yamlls",
@@ -120,7 +206,17 @@ return {
         },
       }
 
-      lspconfig["bashls"].setup(defaultCfg)
+      local rubylsCfg = {}
+      for k, v in pairs(defaultCfg) do
+        rubylsCfg[k] = v
+      end
+
+      rubylsCfg["on_attach"] = function(client, buffer)
+        on_attach(client, buffer)
+        setup_diagnostics(client, buffer)
+        add_ruby_deps_command(client, buffer)
+      end,
+
       lspconfig["lua_ls"].setup(defaultCfg)
       lspconfig["golangci_lint_ls"].setup(defaultCfg)
       lspconfig["gopls"].setup(defaultCfg)
@@ -131,7 +227,8 @@ return {
       lspconfig["marksman"].setup(defaultCfg)
       lspconfig["pylsp"].setup(defaultCfg)
       lspconfig["pyright"].setup(defaultCfg)
-      lspconfig["solargraph"].setup(defaultCfg)
+      -- lspconfig["solargraph"].setup(defaultCfg)
+      lspconfig["ruby_ls"].setup(rubylsCfg)
       lspconfig["terraformls"].setup(defaultCfg)
       lspconfig["yamlls"].setup(yamllsCfg)
     end,
