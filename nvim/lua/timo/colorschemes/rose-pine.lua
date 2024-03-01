3f6648383ac272727c614a98e1132620c6ef0bad
Timo Furrer <tuxtimo@gmail.com>
2024-03-01 18:14:09 +0100

    Change from rose pine back to tokyonight for themes

--- nvim/lua/timo/colorschemes/rose-pine.lua
+++ nvim/lua/timo/colorschemes/rose-pine.lua
@@ -1,18 +1,18 @@
 -- See https://github.com/rose-pine/neovim
 
 return {
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    lazy = false,  -- make sure we load this during startup, because it's the main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    opts = {
-      -- style = "day",
-    },
-    config = function(_, opts)
-      require("rose-pine").setup(opts)
-      -- load the colorscheme
-      vim.cmd([[colorscheme rose-pine-dawn]])
-    end
-  }
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   lazy = false,  -- make sure we load this during startup, because it's the main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   opts = {
+  --     -- style = "day",
+  --   },
+  --   config = function(_, opts)
+  --     require("rose-pine").setup(opts)
+  --     -- load the colorscheme
+  --     vim.cmd([[colorscheme rose-pine-dawn]])
+  --   end
+  -- }
 }
--- nvim/lua/timo/colorschemes/tokyonight.lua
+++ nvim/lua/timo/colorschemes/tokyonight.lua
@@ -1,15 +1,14 @@
 -- See https://github.com/folke/tokyonight.nvim
-return {}
--- return {
---   "folke/tokyonight.nvim",
---   lazy = false, -- make sure we load this during startup, because it's the main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   opts = {
---     -- style = "day",
---   },
---   config = function(_, opts)
---     require("tokyonight").setup(opts)
---     -- load the colorscheme
---     vim.cmd([[colorscheme tokyonight]])
---   end
--- }
+return {
+  "folke/tokyonight.nvim",
+  lazy = false, -- make sure we load this during startup, because it's the main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  opts = {
+    -- style = "day",
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    -- load the colorscheme
+    vim.cmd([[colorscheme tokyonight]])
+  end
+}
