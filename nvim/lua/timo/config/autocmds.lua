56aecb007f91953b0e7121896f13c0fc66451f6b
Timo Furrer <tuxtimo@gmail.com>
2024-04-12 08:52:11 +0200

    Autocmd for opening file at last pos

---
+++ nvim/lua/timo/config/autocmds.lua
@@ -0,0 +1,9 @@
+-- Open a file at the position it was last closed
+vim.api.nvim_create_autocmd({"BufReadPost"}, {
+    pattern = {"*"},
+    callback = function()
+        if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+            vim.api.nvim_exec("normal! g'\"",false)
+        end
+    end
+})
--- nvim/lua/timo/config/init.lua
+++ nvim/lua/timo/config/init.lua
@@ -1,2 +1,4 @@
 require("timo.config.options")
 require("timo.config.keymaps")
+require("timo.config.autocmds")
+require("timo.config.diagnostics")
