6718f28599b06b8664d1f7ce960e5658b9782b83
Timo Furrer <tuxtimo@gmail.com>
2025-08-14 09:56:57 +0200

    switch to neovim-golang

--- nvim/lua/timo/plugins/neotest.lua
+++ nvim/lua/timo/plugins/neotest.lua
@@ -15,7 +15,7 @@ return {
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
       -- Test Runners
-      "nvim-neotest/neotest-go",
+      "fredrikaverpil/neotest-golang",
       "mrcjkb/neotest-haskell",
       "olimorris/neotest-rspec",
       -- DAP
@@ -24,13 +24,11 @@ return {
     opts = function()
       return {
         adapters = {
-          require("neotest-go")({
-            experimental = { test_table = true },
-          }),
+          require("neotest-golang")({}),
           require("neotest-haskell"),
           require("neotest-rspec")({
             rspec_cmd = function()
-              return vim.tbl_flatten({"bundle", "exec", "rspec"})
+              return vim.tbl_flatten({ "bundle", "exec", "rspec" })
             end,
           })
         },
@@ -50,22 +48,20 @@ return {
       require("neotest").setup(opts)
     end,
     keys = {
-      { '<leader>r', desc = 'Neotest' },
-      { '<leader>rt', function() require('neotest').run.run() end, desc = 'Run the nearest test' },
+      { '<leader>r',  desc = 'Neotest' },
+      { '<leader>rt', function() require('neotest').run.run() end,      desc = 'Run the nearest test' },
       { '<leader>rw', function() require('neotest').watch.toggle() end, desc = 'Toggle watching the nearest test' },
-      { '<leader>rd', function()
-        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        if buf_ft == "go" then
-          -- neotest-go doesn't yet support the DAP strategy, thus, we have to use another keybinding
-          -- See https://github.com/nvim-neotest/neotest-go/issues/12
-          require('dap-go').debug_test()
-        else
+      {
+        '<leader>rd',
+        function()
+          local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
           require('neotest').run.run({ strategy = 'dap' })
-        end
-      end, desc = 'Debug the nearest test' },
-      { '<leader>rf', function() require('neotest').run.run(vim.fn.expand('%')) end, desc = 'Run the current file' },
-      { '<leader>rl', function() require('neotest').run.run_last() end, desc = 'Repeat last test run' },
-      { '<leader>rr', function() require('neotest').summary.toggle() end, desc = 'Open test summary' },
+        end,
+        desc = 'Debug the nearest test'
+      },
+      { '<leader>rf', function() require('neotest').run.run(vim.fn.expand('%')) end,   desc = 'Run the current file' },
+      { '<leader>rl', function() require('neotest').run.run_last() end,                desc = 'Repeat last test run' },
+      { '<leader>rr', function() require('neotest').summary.toggle() end,              desc = 'Open test summary' },
       { '<leader>ro', function() require('neotest').output.open({ enter = true }) end, desc = 'Open test output' },
     }
   }
